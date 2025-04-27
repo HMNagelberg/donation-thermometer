3909ac432d5feb19d81b0fefcf31d7c9ca7673b1
HMNagelberg <hmnagelberg@gmail.com>
2025-04-27 15:31:00 +0300

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import DonationThermometer from './DonationThermometer';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DonationThermometer />
+  </React.StrictMode>
+);
